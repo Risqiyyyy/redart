4fa6d787fdad7561f28b6a7984d422d6d6966552
Risqi Andryan <risqiyt12@gmail.com>
2022-11-03 18:41:16 +0700

    update

---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+
+        <link rel="stylesheet" href="{{ ('assets/css/animate.css') }}" />
+        <link rel="stylesheet" href="assets/css/tailwind.css" />
+
+        <!-- ==== WOW JS ==== -->
+        <script src="assets/js/wow.min.js"></script>
+        <script>
+        new WOW().init();
+        </script>
+    </head>
+    
